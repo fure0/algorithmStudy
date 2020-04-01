6153198f05b102d629c0000b3c8733d5da115e74
KimTaeyoun <kimty1121@gmail.com>
2020-04-02 00:01:12 +0900

    add const type

--- search/Kuruskal.cpp
+++ search/Kuruskal.cpp
@@ -32,7 +32,7 @@ public:
     int node[2];
     int distance;
     Edge(int a, int b, int distance) {
-        this->node[a] = a;
+        this->node[0] = a;
         this->node[1] = b;
         this->distance = distance;
     }
@@ -41,10 +41,13 @@ public:
     }
 };
 
-int main(void) {
-    int n = 7;
-    int m = 11;
 
+
+int main(void) {
+
+    const int n = 7;
+    const int m = 11;
+    
     vector<Edge> v;
     v.push_back(Edge(1, 7, 12));
     v.push_back(Edge(1, 4, 28));
@@ -63,16 +66,16 @@ int main(void) {
 
     // 각 정점이 포함된 그래프가 어디인지 저장
     int set[n];
-    for(int i=0; i<n; i++){
+    for(int i = 0; i < n; i++){
         set[i] = i;
     }
 
     // 거리의 합을 0으로 초기화
     int sum = 0;
 
-    for(int i=0; i<v.size(); i++) {
+    for(int i = 0; i < v.size(); i++) {
         //동일한 부모를 가르키지 않는 경우, 즉 사이클이 발생하지 않을 때만 선택
-        if(!find(set, v[i].node[0] - 1, v[i].node[1] - 1)) {
+        if(!findParent(set, v[i].node[0] - 1, v[i].node[1] - 1)) {
             sum += v[i].distance;
             unionParent(set, v[i].node[0] - 1, v[i].node[1] -1);
         }
